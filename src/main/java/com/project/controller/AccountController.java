c09843d2357ce1eb3b1ee79d07114a41c2c9ad2c
zzsanshi <zzsanshi@163.com>
2021-07-17 22:22:18 +0800

    7/17

--- src/main/java/com/project/controller/AccountController.java
+++ src/main/java/com/project/controller/AccountController.java
@@ -1,5 +1,7 @@
 package com.project.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.project.domain.Account;
 import com.project.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,12 +24,35 @@ public class AccountController {
     private AccountService accountService;
 
     @RequestMapping("/account/findAll")
-    public String findAll(Model model) {  //存数据， Model对象
+    public String findAll(@RequestParam(name = "page", defaultValue = "1") Integer page, Model model) {  //存数据， Model对象
         System.out.println("Controller表现层：查询所有账户...");
+        PageHelper.startPage(page, 3);//每页条数=3
         // 调用service的方法
         List<Account> list = accountService.findAll();
+
+
+        //查看一下是否查到了数据
+        for (Account user : list) {
+            System.out.println("=========" + user);
+        }
+        /**
+         * pageNum:当前页
+         * pageSize:每页的数量
+         * size:当前页的数量
+         * pages:总页数
+         * total:总记录数
+         */
+
+        /**
+         *  PageInfo就是当初那个分页工具类(分页bean)
+         */
+        PageInfo<Account> pageInfo = new PageInfo<Account>(list);//把数据放在分页类中
+        //封装数据
+        //传分页类
+        model.addAttribute("pageInfo", pageInfo);
         model.addAttribute("list1", list);
         return "list";
+
     }
 
     @RequestMapping("/account/save")
--- src/main/webapp/WEB-INF/pages/list.jsp
+++ src/main/webapp/WEB-INF/pages/list.jsp
@@ -1,6 +1,4 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-
-
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 
 <html>
@@ -9,9 +7,30 @@
 </head>
 <body>
 <h2>查询所有的账户</h2>
-<c:forEach items="${list1}" var="account">
-    姓名:${account.name}   id:${account.id}  密码:${account.password}<br/>
-    <br/>
-</c:forEach>
+<h3 align="left">分页查询</h3>
+<!-- 数据部分 -->
+<table  align="left" border="1px solid pink" style="text-align: center;font-size: 14px;margin-top:0px;" >
+    <tbody>
+    <tr>
+        <td>用户编号</td>
+        <td>用户姓名</td>
+        <td>用户密码</td>
+    </tr>
+    <c:forEach items="${list}" var="account">
+        <tr>
+            <td>${account.id}</td>
+            <td>${account.name}</td>
+            <td>${account.password}</td>
+        </tr>
+    </c:forEach>
+    </tbody>
+</table>
+<br>
+<!-- 分页按钮部分 -->
+<p>当前是${pageInfo.pageNum}/${pageInfo.pages}</p>
+<a href="/account/findAll?page=${pageInfo.firstPage}">第一页</a>
+<a href="/account/findAll?page=${pageInfo.nextPage}">下一页</a>
+<a href="/account/findAll?page=${pageInfo.prePage}">上一页</a>
+<a href="/account/findAll?page=${pageInfo.lastPage}">最后页</a>
 </body>
 </html>
